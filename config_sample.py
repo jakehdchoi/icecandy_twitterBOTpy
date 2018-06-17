97ba96ab4cf691d536cab0b9b714d7e70ed3a4bf
jakehdchoi <jakehdchoi@gmail.com>
2018-06-18 00:47:54 +0900

    config file added

---
+++ config_sample.py
@@ -0,0 +1,8 @@
+userList = [
+    'screen_name1',
+    'screen_name2'
+]
+
+count = 5
+exclude_replies = 'false'
+include_rts = 'true'
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import requests
 from requests_oauthlib import OAuth1
 
-
+from config import *
 from credentials import *
 
 
@@ -13,20 +13,6 @@ from credentials import *
 url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
 auth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
 requests.get(url, auth=auth)
-
-
-userList = [
-    'BTCTN',
-    'rogerkver',
-    'VitalikButerin',
-    'justinsuntron',
-    'Huobi_Pro',
-    'jakehdchoi'
-]
-
-count = 5
-exclude_replies = 'false'
-include_rts = 'true'
 
 
 
